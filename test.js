edec23f9ad2568708ad4c7894f592d0bbcc90f74
Antti <anttituomola8@gmail.com>
2022-01-06 14:12:08 +0200

    basic functionality

--- test.js
+++ test.js
@@ -2,33 +2,33 @@ const canvas = document.getElementById("lostRobot")
 const canvasFill = lostRobot.getContext("2d")
 let copyPaste = `
 ##################################################
-#.................#.#.......#.............#......#
-#.................E...........................#..#
+#................................................#
+#.......................#............#...........#
+#..................#......#................#.....#
+#.............................................#..#
+#......................#.........................#
+#.................................#..........#...#
+#.............................................#..#
+#...........#.....#..............................#
+#.......................#........#...........#...#
+#............................#............#......#
+#.................#.................#............#
+#..........................................#.....#
+#................................................#
+#................................................#
+#................................................#
+#..........#..........................#..........#
+#..................................#..........#..#
+#...................#.......#....................#
+#.................E..........................#...#
+#................................................#
+#...................................#............#
+#.........................................#......#
 #..................#.............................#
-#..........................#.....................#
-#................................................#
-#................#...............................#
-#........................................#.......#
-#................................................#
-#.....................................#..........#
-#.......................S....................#...#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
-#................................................#
+#.................S.#....#...........#...........#
+#..........................#................#....#
+#..................#....#........................#
+#......................#..#......................#
 #................................................#
 ##################################################`
 
@@ -44,6 +44,8 @@ let canvasWidth = trimmedCanvasContent.length / 3
 let canvasHeight = trimmedCanvasContent.length / 5
 let startingPoint = 0
 let robotDirection = "up"
+let robotLocation
+let count = 0
 
 
 drawCanvas()
@@ -51,33 +53,34 @@ drawCanvas()
 function drawCanvas() {
     let x = 0
     let y = 0
-    let index = 0
-    
-    while (index < trimmedCanvasContent.length) {
+    let drawIndex = 0
+
+    while (drawIndex < trimmedCanvasContent.length) {
         for (const el of canvasArray) {
             if (el === ".") {
                 canvasFill.fillStyle = boardFill
                 canvasFill.fillRect(x, y, 10, 10)
                 canvasFill.strokeRect(x, y, 10, 10)
             }
-            if (el === "#") {
+            else if (el === "#") {
                 canvasFill.fillStyle = obstacleColor
                 canvasFill.fillRect(x, y, 10, 10)
                 canvasFill.strokeRect(x, y, 10, 10)
             }
-            if (el === "S") {
+            else if (el === "S") {
                 canvasFill.fillStyle = "green"
                 canvasFill.fillRect(x, y, 10, 10)
                 canvasFill.strokeRect(x, y, 10, 10)
-                startingPoint = index
+                startingPoint = drawIndex
             }
-            if (el === "E") {
+            else if (el === "E") {
                 canvasFill.fillStyle = "pink"
                 canvasFill.fillRect(x, y, 10, 10)
                 canvasFill.strokeRect(x, y, 10, 10)
             }
+
             x += 10
-            index++
+            drawIndex++
             if (x === canvasWidth && y !== canvasHeight) {
                 y += 10
                 x = 0
@@ -87,52 +90,75 @@ function drawCanvas() {
     decideRobotAction(startingPoint)
 }
 
-function turnRobot() {
-    if (robotDirection === "up") {
-        robotLocation += 1
-        robotDirection = "right"
-    }
-    if (robotDirection === "right") {
-        robotLocation += 30
-        robotDirection = "down"
-    }
-    if (robotDirection === "down") {
-        robotLocation -= 1
-        robotDirection = "left"
-    }
-    if (robotDirection === "left") {
-        robotLocation -= 30
-        robotDirection = "up"
-    }
-    moveRobot()
-}
-
-function moveRobot(robotLocation) {
-    if (robotDirection === "up") {
-        robotLocation += 1
-    }
-    if (robotDirection === "right") {
-        robotLocation += 30
-    }
-    if (robotDirection === "down") {
-        robotLocation -= 1
-    }
-    if (robotDirection === "left") {
-        robotLocation -= 30
-    }
-}
-
+// What's under the robot now?
 function decideRobotAction(robotLocation) {
     if (canvasArray[robotLocation] === ".") {
-        moveRobot()
-    }
-    if (canvasArray[robotLocation] === "#") {
-        turnRobot()
-    }
-    if (canvasArray[robotLocation] === "S") {
         moveRobot(robotLocation)
     }
-    if (canvasArray[robotLocation] === "E") {
-        alert("HERE")
+    else if (canvasArray[robotLocation] === "#") {
+        turnRobot(robotLocation)
     }
+    else if (canvasArray[robotLocation] === "S") {
+        moveRobot(robotLocation)
+    }
+    else if (canvasArray[robotLocation] === "E") {
+        console.log(count)
+        return count
+    }
+}
+
+// Move robot
+function moveRobot(robotLocation) {
+    if (count < 1000) {
+        if (robotDirection === "up") {
+            robotLocation -= 50
+        }
+        else if (robotDirection === "right") {
+            robotLocation += 1
+        }
+        else if (robotDirection === "down") {
+            robotLocation += 50
+        }
+        else if (robotDirection === "left") {
+            robotLocation -= 1
+        }
+        count++
+        figureOutLocationFromIndex(robotLocation)
+        decideRobotAction(robotLocation)
+    } else if (count >= 1000) {
+        console.log(`Tried ${count} times, no luck!`)
+    }
+}
+
+// Turn robot
+function turnRobot(robotLocation) {
+    if (robotDirection === "up") {
+        robotDirection = "right"
+    }
+    else if (robotDirection === "right") {
+        robotDirection = "down"
+    }
+    else if (robotDirection === "down") {
+        robotDirection = "left"
+    }
+    else if (robotDirection === "left") {
+        robotDirection = "up"
+    }
+    moveRobot(robotLocation)
+}
+
+
+
+function figureOutLocationFromIndex(index) {
+    const rows = 30
+    const colums = 50
+    let x = 0 
+    let y = 0
+    const remainder = index % colums
+    const rowCount = (index - remainder) / colums
+    y = rowCount * 10
+    x = remainder * 10
+    canvasFill.fillStyle = "black"
+    canvasFill.fillRect(x, y, 10, 10)
+    canvasFill.strokeRect(x, y, 10, 10)
 }
