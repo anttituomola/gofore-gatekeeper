3cd2a8e67c414eab86d328ff093e9b0ba1d0f380
Antti <anttituomola8@gmail.com>
2022-01-07 14:15:33 +0200

    classes

--- helpers/GetInput.js
+++ helpers/GetInput.js
@@ -1,26 +1,27 @@
-const inputValue = document.getElementById("inputCanvas").value
 
-// Remove line breaks from the content to get real length
-const trimmedCanvasContent = inputValue.replace(/(\r\n|\n|\r)/gm, "")
-let canvasArray = trimmedCanvasContent.split("")
-
-const canvasId = 0
-const canvasWidth = 0
-const canvasHeight = 0
+import {columns, rows, squareSide} from "../helpers/Draw.js"
+import Draw from "./Draw.js"
+const draw = new Draw()
 
 
-export class GetInput {
-    getinput() {
+export default class GetInput {
+    getInput() {
+        // Remove line breaks from the content to get real length
+        const inputValue = document.getElementById("inputCanvas").value
+        const trimmedCanvasContent = inputValue.replace(/(\r\n|\n|\r)/gm, "")
+        let canvasArray = trimmedCanvasContent.split("")
+
         // Get the ID out of the input
-        this.canvasId = canvasArray.slice(0, 7)
+        const canvasId = canvasArray.slice(0, 7)
 
         // Get the canvas out of the input
         canvasArray = canvasArray.splice(7, canvasArray.length)
 
-        this.canvasWidth = canvasArray.length / (rows / squareSide)
-        this.canvasHeight = canvasArray.length / (colums / squareSide)
+        const canvasWidth = canvasArray.length / (columns / squareSide)
+        const canvasHeight = canvasArray.length / (rows / squareSide)
 
-        drawCanvas(canvasArray, canvasId)
+        draw.drawCanvas(canvasArray, canvasId, canvasWidth, canvasHeight)
 
     }
 }
+
