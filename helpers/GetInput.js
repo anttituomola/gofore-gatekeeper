d7852d1f162fc9d42172006142223c9999491f7a
Antti <anttituomola8@gmail.com>
2022-01-08 20:42:47 +0200

    validate input length

--- helpers/GetInput.js
+++ helpers/GetInput.js
@@ -6,22 +6,29 @@ const draw = new Draw()
 
 export default class GetInput {
     getInput() {
+        const validInputLength = 1507
         // Remove line breaks from the content to get real length
         const inputValue = document.getElementById("inputCanvas").value
         const trimmedCanvasContent = inputValue.replace(/(\r\n|\n|\r)/gmu, "")
         let canvasArray = trimmedCanvasContent.split("")
 
-        // Get the ID out of the input
-        const canvasId = canvasArray.slice(0, 7)
-
-        // Get the canvas out of the input
-        canvasArray = canvasArray.splice(7, canvasArray.length)
-
-        const canvasWidth = canvasArray.length / (columns / squareSide)
-        const canvasHeight = canvasArray.length / (rows / squareSide)
-
-        draw.drawCanvas(canvasArray, canvasId, canvasWidth, canvasHeight)
-
+        // Make sure input is valid in length
+        if (canvasArray.length === validInputLength) {
+            // Get the ID out of the input
+            const canvasId = canvasArray.slice(0, 7)
+    
+            // Get the canvas out of the input
+            canvasArray = canvasArray.splice(7, canvasArray.length)
+    
+            const canvasWidth = canvasArray.length / (columns / squareSide)
+            const canvasHeight = canvasArray.length / (rows / squareSide)
+    
+            draw.drawCanvas(canvasArray, canvasId, canvasWidth, canvasHeight)
+        } else if (canvasArray.length < validInputLength) {
+            document.getElementById("answer").innerHTML = `<p>You input was too short. Did you include the ID number as well?</p>`
+        } else if (canvasArray.length > validInputLength) {
+            document.getElementById("answer").innerHTML = `<p>You input was too long, please check your copy-paste.</p>`
+        }
     }
 }
 
